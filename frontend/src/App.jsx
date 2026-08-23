2be630937c5230e2fe940e895839989d61c8f459
Rajesh190602 <martharajesh76@gmail.com>
2026-08-23 13:01:41 +0530

    Test admin users import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
-import AdminLands from "./pages/admin/AdminLands";
+import Users from "./pages/admin/Users";
 
 function App() {
   return (
