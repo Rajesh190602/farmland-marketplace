37ef372962da4c64548b8d796b08690777b67af7
Rajesh190602 <martharajesh76@gmail.com>
2026-08-23 12:51:57 +0530

    Test public page imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import AddLand from "./pages/AddLand";
+import MyLands from "./pages/MyLands";
+import EditLand from "./pages/EditLand";
+import AllLands from "./pages/AllLands";
+import SearchLands from "./pages/SearchLands";
 
 function App() {
   return (
