d18f5ea7086cd41779310bfb8d81371250ded463
Rajesh190602 <martharajesh76@gmail.com>
2026-08-23 12:58:38 +0530

    Test admin lands import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
-import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminLands from "./pages/admin/AdminLands";
 
 function App() {
   return (
