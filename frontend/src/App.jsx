c886cf292dc1ae235d9c08a9e53a711f00f5cd08
Rajesh190602 <martharajesh76@gmail.com>
2026-08-23 12:50:01 +0530

    Test Login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function TestPage() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "32px",
-        fontWeight: "bold",
-        color: "green",
-      }}
-    >
-      REACT ROUTER IS WORKING
-    </div>
-  );
-}
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<TestPage />} />
+        <Route path="*" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
